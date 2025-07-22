501cfacfc72410f69cc07585515a46d229700c3d
Markus Günther <markus@flownative.com>
2025-07-22 14:34:36 +0200

    FEATURE: Extract node update to separate method
    As it can happen that you may not use the PublishingService in the Backend, you probably don’t have a BackendUser. So this change extracts the update of the node to an extra method that just gets a string for the current publisher.
    
    We also define the maximum items via setting.

--- Classes/Service/PublishingService.php
+++ Classes/Service/PublishingService.php
@@ -7,9 +7,11 @@ namespace Flownative\LastPublisher\Service;
  * This file is part of the Flownative.LastPublisher package.
  */
 
+use Neos\ContentRepository\Exception\NodeException;
 use Neos\Flow\Annotations as Flow;
 use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\ContentRepository\Domain\Model\Workspace;
+use Neos\Neos\Domain\Model\User;
 use Neos\Neos\Service\PublishingService as NeosPublishingService;
 use Neos\Neos\Service\UserService;
 use Psr\Log\LoggerInterface;
@@ -38,6 +40,12 @@ final class PublishingService
     protected UserService $userService;
 
     /**
+     * @Flow\InjectConfiguration(package="Flownative.LastPublisher", path="PublishingHistoryEditor.maximumItems")
+     * @var integer
+     */
+    protected $maximumItems;
+
+    /**
      * Publishes the given node to the specified target workspace. If no workspace is specified, the base workspace
      * is assumed.
      *
@@ -50,52 +58,59 @@ final class PublishingService
      */
     public function persistLastPublisherForNode(NodeInterface $node, ?Workspace $targetWorkspace = null)
     {
-        $this->logger->debug(
-            sprintf('Persisting last publisher information for node "%s" in workspace "%s"', $node->getNodeAggregateIdentifier(), $targetWorkspace?->getName() ?? 'default')
-        );
         if ($targetWorkspace === null) {
             $targetWorkspace = $node->getWorkspace()->getBaseWorkspace();
         }
 
+        if ($this->userService === null) {
+            $this->logger->warning(
+                'UserService is not available, cannot persist last publishing information for node "{nodeIdentifier}" in workspace "{workspaceName}".',
+                [
+                    'nodeIdentifier' => $node->getNodeAggregateIdentifier(),
+                    'workspaceName' => $targetWorkspace->getName()
+                ]
+            );
+            return;
+        }
+
+        $currentUser = $this->userService->getBackendUser();
+        $this->appendLastPublisherForNode($node, $targetWorkspace, $currentUser->getName()->getFullName());
+    }
+
+    /**
+     * Appends the last publisher information to the node's properties.
+     *
+     * @param NodeInterface $node
+     * @param Workspace|null $targetWorkspace
+     * @param string $publisherName
+     * @return void
+     * @throws NodeException
+     */
+    public function appendLastPublisherForNode(NodeInterface $node, ?Workspace $targetWorkspace = null, string $publisherName = ''): void
+    {
         $nodeType = $node->getNodeType();
-
         if ($nodeType->isOfType('Flownative.LastPublisher:Mixin.LastPublisher')) {
-            if ($this->userService === null) {
-                $this->logger->warning(
-                    'UserService is not available, cannot persist last publishing information for node "{nodeIdentifier}" in workspace "{workspaceName}".',
-                    [
-                        'nodeIdentifier' => $node->getNodeAggregateIdentifier(),
-                        'workspaceName' => $targetWorkspace->getName()
-                    ]
-                );
-                return;
+            $publishingInfo = $node->getProperty('last_publishing_information') ?: [];
+            $publishingInfo[] = [
+                'targetWorkspace' => $targetWorkspace->getTitle(),
+                'publisher' => $publisherName,
+                'publishingDate' => (new \DateTime())->format('c')
+            ];
+
+            if (count($publishingInfo) > $this->maximumItems) {
+                $publishingInfo = array_slice($publishingInfo, -($this->maximumItems));
             }
 
-            $currentUser = $this->userService->getBackendUser();
-            if ($currentUser !== null) {
-                $publisherName = $currentUser->getName()->getFullName();
-                $publishingInfo = $node->getProperty('last_publishing_information') ?: [];
-                $publishingInfo[] = [
-                    'targetWorkspace' => $targetWorkspace->getTitle(),
-                    'publisher' => $publisherName,
-                    'publishingDate' => (new \DateTime())->format('c')
-                ];
+            $this->logger->info(
+                sprintf(
+                    'Persisting last publishing information for node "%s" in workspace "%s" by user "%s"',
+                    $node->getNodeAggregateIdentifier(),
+                    $targetWorkspace->getName(),
+                    $publisherName
+                )
+            );
 
-                if (count($publishingInfo) > 5) {
-                    $publishingInfo = array_slice($publishingInfo, -5);
-                }
-
-                $this->logger->debug(
-                    sprintf(
-                        'Persisting last publishing information for node "%s" in workspace "%s" by user "%s"',
-                        $node->getNodeAggregateIdentifier(),
-                        $targetWorkspace->getName(),
-                        $publisherName
-                    )
-                );
-
-                $node->setProperty('last_publishing_information', $publishingInfo);
-            }
+            $node->setProperty('last_publishing_information', $publishingInfo);
         }
     }
 }
